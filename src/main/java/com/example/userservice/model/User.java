f24902ff15ac940a131eccd3390cd24d1a6d03e0
ccousill <43454787+ccousill@users.noreply.github.com>
2022-11-01 13:36:17 -0400

    Initialization

---
+++ src/main/java/com/example/userservice/model/User.java
@@ -0,0 +1,22 @@
+package com.example.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String firstName;
+    private String email;
+    private Long departmentId;
+
+}
---
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,39 @@
+package com.example.userservice.service;
+
+import com.example.userservice.VO.Department;
+import com.example.userservice.VO.ResponseTemplateVO;
+import com.example.userservice.model.User;
+import com.example.userservice.repository.UserRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User saveUser(User user) {
+        log.info("inside saveUser of UserService");
+        return repo.save(user);
+    }
+
+
+    public ResponseTemplateVO getUserWithDepartment(Long userId) {
+        log.info("inside getuserwithdepartment of UserService");
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        User user = repo.findByUserId(userId);
+        Department department = restTemplate.getForObject("http://localhost:9001/departments/"+ user.getDepartmentId(),Department.class);
+        vo.setUser(user);
+        vo.setDepartment(department);
+        return vo;
+
+
+    }
+}
